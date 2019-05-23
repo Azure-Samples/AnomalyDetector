adf2b07e5d405463d9cab0a9970f945e4788cf6c
Aaron Hill <aahi@microsoft.com>
2019-05-23 13:19:28 -0700

    updating python sample

--- quickstarts/sdk/python-sdk-sample.py
+++ quickstarts/sdk/python-sdk-sample.py
@@ -1,57 +1,96 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+
+# This sample demonstrates the Anomaly Detection service's two detection methods:
+#    * Anomaly detection on an entire time-series dataset.
+#    * Anomaly detection on the latest data point in a dataset.
+
+# * Prerequisites:
+#     * The Anomaly Detector client library for Python
+#     * A .csv file containing a time-series data set with 
+#        UTC-timestamp and numerical values pairings. 
+#        Example data is included in this repo.
+
+# <imports>
 from azure.cognitiveservices.anomalydetector import AnomalyDetectorClient
 from azure.cognitiveservices.anomalydetector.models import Request, Point, Granularity, \
     APIErrorException
 from msrest.authentication import CognitiveServicesCredentials
 import pandas as pd
+import os
+# </imports>
+
+# <initVars>
+# Set the COGSRV_ACCOUNT_KEY environment variable on your local machine using the
+# appropriate method for your preferred shell (Bash, PowerShell, Command Prompt, etc.)
+SUBSCRIPTION_KEY = os.environ["COGSRV_ACCOUNT_KEY"]
+ANOMALY_DETECTION_LOCATION = "westus2" #eastus?
+TIME_SERIES_DATA_PATH = ""
+# </initVars>
+
+# Create an Anomaly Detector client and add the 
+
+# <client>
+base_url = "https://{}.api.cognitive.microsoft.com".format(ANOMALY_DETECTION_LOCATION)
+client = AnomalyDetectorClient(base_url, CognitiveServicesCredentials(SUBSCRIPTION_KEY))
+# </client>
+
+# Load in the time series data file
+
+# <loadDataFile>
+series = []
+data_file = pd.read_csv(TIME_SERIES_DATA_PATH, header=None, encoding='utf-8', parse_dates=[0])
+for index, row in data_file.iterrows():
+    series.append(Point(timestamp=row[0], value=row[1]))
+# </loadDataFile>
+
+# Create a request from the data file 
+
+# <request>
+request = Request(series=series, granularity=Granularity.daily)
+# </request>
 
 
-def entire_detect_sample(endpoint, key, request):
-    print('Sample of detecting anomalies in the entire series.')
+# detect anomalies throughout the entire time series, as a batch
 
-    client = AnomalyDetectorClient(endpoint, CognitiveServicesCredentials(key))
+# <detectAnomaliesBatch>
+print('Detecting anomalies in the entire time series.')
+
+try:
     response = client.entire_detect(request)
-    if True in response.is_anomaly:
-        print('Anomaly was detected from the series at index:')
-        for i in range(len(series)):
-            if response.is_anomaly[i]:
-                print(i)
+except Exception as e:
+    if isinstance(e, APIErrorException):
+        print('Error code: {}'.format(e.error.code),
+            'Error message: {}'.format(e.error.message))
     else:
-        print('There is no anomaly detected from the series.')
+        print(e)
 
+if True in response.is_anomaly:
+    print('An anomaly was detected at index:')
+    for i in range(len(response.is_anomaly)):
+        if response.is_anomaly[i]:
+            print(i)
+else:
+    print('No anomalies were detected in the time series.')
+# </detectAnomaliesBatch>
 
-def last_detect_sample(endpoint, key, request):
-    print('Sample of detecting whether the latest point in series is anomaly.')
+# Detect the anomaly status of the latest data point
 
-    client = AnomalyDetectorClient(endpoint, CognitiveServicesCredentials(key))
+# <latestPointDetection>
+print('Detecting the anomaly status of the latest data point.')
+
+try:
     response = client.last_detect(request)
-    if response.is_anomaly:
-        print('The latest point is detected as anomaly.')
+except Exception as e:
+    if isinstance(e, APIErrorException):
+        print('Error code: {}'.format(e.error.code),
+            'Error message: {}'.format(e.error.message))
     else:
-        print('The latest point is not detected as anomaly.')
+        print(e)
 
-
-def get_series_from_file(path):
-    df = pd.read_csv(path, header=None, encoding='utf-8', parse_dates=[0])
-    series = []
-    for index, row in df.iterrows():
-        series.append(Point(timestamp=row[0], value=row[1]))
-    return series
-
-
-if __name__ == '__main__':
-    endpoint = '[YOUR_ENDPOINT_URL]'
-    key = '[YOUR_SUBSCRIPTION_KEY]'
-    path = '[PATH_TO_TIME_SERIES_DATA]'
-
-    try:
-        series = get_series_from_file(path)
-        request = Request(series=series, granularity=Granularity.daily)
-
-        entire_detect_sample(endpoint, key, request)
-        last_detect_sample(endpoint, key, request)
-    except Exception as e:
-        if isinstance(e, APIErrorException):
-            print('Error code: {}'.format(e.error.code))
-            print('Error message: {}'.format(e.error.message))
-        else:
-            print(e)
+if response.is_anomaly:
+    print('The latest point is detected as anomaly.')
+else:
+    print('The latest point is not detected as anomaly.')
+# </latestPointDetection>
