f9fc3de799492bbeaf3f868241c041d0a1d82b3e
hongli750210 <v-hongli1@microsoft.com>
2021-11-15 13:05:36 +0800

    fixed comments for PR#1 of AnomalyDetector 202111151305

--- other-samples/java-sample/src/main/java/com/microsoft/cognitiveservice/anomalydetection/Series.java
+++ other-samples/java-sample/src/main/java/com/microsoft/cognitiveservice/anomalydetection/Series.java
@@ -2,23 +2,22 @@ package com.microsoft.cognitiveservice.anomalydetection;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.auto.value.AutoValue;
 
-@AutoValue
-public abstract class Series {
-
-    public static final String TIEMSTAMP = "timestamp";
-    public static final String VALUE = "value";
+public class Series {
+    private final String timestamp;
+    private final int value;
 
     @JsonCreator
-    public static Series create(@JsonProperty(TIEMSTAMP) String timestamp, @JsonProperty(VALUE) Float value) {
-        return new com.microsoft.cognitiveservice.anomalydetection.AutoValue_Series(timestamp, value);
+    public Series(@JsonProperty("timestamp") String timestamp, @JsonProperty("value") int value) {
+        this.timestamp = timestamp;
+        this.value = value;
     }
 
-    @JsonProperty(TIEMSTAMP)
-    public abstract String timestamp();
+    public String getTimestamp() {
+        return timestamp;
+    }
 
-    @JsonProperty(VALUE)
-    public abstract Float value();
-
+    public int getValue() {
+        return value;
+    }
 }
